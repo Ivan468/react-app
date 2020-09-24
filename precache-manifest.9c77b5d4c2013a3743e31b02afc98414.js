35a51b596f38e48a8ffeb74c639020a3dcff18f9
Ivan <pig1995z@gmailcom>
2020-09-24 22:13:49 +0300

    Updates

---
+++ precache-manifest.9c77b5d4c2013a3743e31b02afc98414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ca95ca2e287508cd7fb70708078c82",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "cc29d5d99518fa9f8504",
+    "url": "/react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c583c2a87708ed3f3f74",
+    "url": "/react-app/static/js/2.776a23e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.776a23e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc29d5d99518fa9f8504",
+    "url": "/react-app/static/js/main.35920e32.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  }
+]);
